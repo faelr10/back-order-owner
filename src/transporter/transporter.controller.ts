9970072bae2519f9e5711b92f1e4912acbef9135
Rafael Victor <rafaelv.boscato@hotmail.com>
2023-12-07 18:15:54 -0300

    🚧 update transporter

--- src/transporter/transporter.controller.ts
+++ src/transporter/transporter.controller.ts
@@ -1,14 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { request_order_payload } from './transporter.config';
+import { IOrderService } from 'src/order/structure/service.structure';
+import { OrderService } from 'src/order/order.service';
 
 @Controller()
 export class TransporterController {
-  //constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    @Inject(OrderService) private readonly orderService: IOrderService,
+  ) {}
 
   @MessagePattern('request_order_topic')
   async RequestOrderTopic(@Payload() message: request_order_payload) {
-    console.log(message);
+    await this.orderService.pendingRequest(message.order_id);
     return 'Success';
   }
 }
--- src/transporter/transporter.module.ts
+++ src/transporter/transporter.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TransporterController } from './transporter.controller';
+import { OrderService } from 'src/order/order.service';
+import { OrderRepository } from 'src/order/order.repository';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [TransporterController],
-  providers: [],
+  providers: [OrderService, OrderRepository, PrismaService],
 })
 export class TransporterModule {}
