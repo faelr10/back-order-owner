e410de81e79108ea3ee1f431a344874ff5a9724f
Rafael Victor <rafaelv.boscato@hotmail.com>
2023-12-11 11:37:11 -0300

    🚧 update main

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { KafkaConfig } from './kafka.config';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    KafkaConfig(),
-  );
-
-  // Inicie o microserviço Kafka
-  await app.listen();
-
-  // Inicie o servidor HTTP
-  const httpApp = await NestFactory.create(AppModule);
-  await httpApp.listen(3001);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
