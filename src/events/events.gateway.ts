4b5dc0c99a359c6b0af44a27e20f2e429f2adcc3
Rafael Victor <rafaelv.boscato@hotmail.com>
2023-12-14 12:59:00 -0300

    🐛 fix a bug

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -2,6 +2,7 @@ import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -9,49 +10,75 @@ import {
 import { Consumer, Kafka, Message, Producer, ProducerRecord } from 'kafkajs';
 import { Server, Socket } from 'socket.io';
 
+const group_id_postman = 'client_postman';
+const group_id_front = 'client_front';
+
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class EventsGateway {
+export class EventsGateway implements OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
   private logger: Logger = new Logger('AppGateway');
   private kafka: Kafka;
-  private kafkaConsumer: Consumer;
   private kafkaProducer: Producer;
+  private kafkaConsumers: Map<string, Consumer> = new Map();
 
   constructor() {
     this.kafka = new Kafka({
       clientId: process.env.KAFKA_CONSUMER_GROUP_ID,
-      brokers: ['localhost:9092', 'kafka-broker2:9092'],
+      brokers: ['localhost:9092'],
     });
-
-    this.kafkaConsumer = this.kafka.consumer({
-      groupId: 'test',
-    });
-
-    this.kafkaConsumer.connect();
-    this.kafkaConsumer.subscribe({ topic: 'request_order_topic' });
 
     this.kafkaProducer = this.kafka.producer();
   }
 
-  handleConnection(client: Socket) {
-    this.logger.log(`New client connection: ${client.id}`);
+  private createKafkaConsumer(groupId: string) {
+    const kafkaConsumer = this.kafka.consumer({
+      groupId,
+    });
 
-    const runKafkaConsumer = async () => {
-      await this.kafkaConsumer.run({
-        eachMessage: async ({ message }) => {
-          const convert_message = JSON.parse(message.value.toString());
-          delete convert_message.origin;
-          client.emit('new_order', convert_message);
-        },
-      });
-    };
-    runKafkaConsumer();
+    kafkaConsumer.connect();
+    kafkaConsumer.subscribe({ topic: 'request_order_topic' });
+
+    kafkaConsumer.run({
+      eachMessage: async ({ message }) => {
+        const convert_message = JSON.parse(message.value.toString());
+        delete convert_message.origin;
+        this.server.emit('new_order', convert_message);
+      },
+    });
+
+    return kafkaConsumer;
+  }
+
+  async handleConnection(client: Socket) {
+    await this.handleDisconnect(client);
+
+    let group_id: string;
+    if (client.handshake.headers.authorization === 'client_postman') {
+      group_id = group_id_postman;
+    } else {
+      group_id = group_id_front;
+    }
+
+    this.logger.log(`New client connection: ${client.id}`);
+    client.join(group_id);
+
+    const kafkaConsumer = this.createKafkaConsumer(group_id);
+    this.kafkaConsumers.set(client.id, kafkaConsumer);
+  }
+
+  async handleDisconnect(client: Socket) {
+    const kafkaConsumer = this.kafkaConsumers.get(client.id);
+
+    if (kafkaConsumer) {
+      await kafkaConsumer.disconnect();
+      this.kafkaConsumers.delete(client.id);
+    }
   }
 
   @SubscribeMessage('order_status')
