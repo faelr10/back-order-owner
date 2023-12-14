55cab5f531be29fddf652185244dcd606af45b33
Rafael Victor <rafaelv.boscato@hotmail.com>
2023-12-14 13:00:02 -0300

    🚧 updated app_module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from '@nestjs/microservices';
 import { Constants, KafkaConfig } from './kafka.config';
-import { TransporterModule } from './transporter/transporter.module';
 import { OrderModule } from './order/order.module';
+import { EventsModule } from './events/events.module';
 
 const kafkaImport = ClientsModule.register([
   KafkaConfig(Constants.KafkaClientToken),
 ]);
 
 @Module({
-  imports: [kafkaImport, TransporterModule, OrderModule],
+  imports: [kafkaImport, OrderModule, EventsModule],
   controllers: [],
   providers: [],
 })
